7c0ace53d67b051147e7e9b4812739cb10e243ff
Johanlai <65450101+Johanlai@users.noreply.github.com>
2023-07-03 21:25:36 +0000

    1-3 challenges

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,34 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+SELECT BookID, Barcode
+FROM Books
+WHERE Title in ('The Picture of Dorian Gray','Great Expectations')
+AND Barcode in (2855934983,4043822646)
+
+SELECT BookID
+FROM Books
+WHERE Title='The Picture of Dorian Gray'
+AND Barcode=2855934983
+
+SELECT PatronID FROM Patrons 
+WHERE FirstName||' '||LastName='Jack Vaan'
+AND Email='jvaan@wisdompets.com'
+
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES 
+((SELECT BookID FROM Books 
+  WHERE Title='The Picture of Dorian Gray'
+  AND Barcode=2855934983),
+  (SELECT PatronID FROM Patrons 
+  WHERE FirstName||' '||LastName='Jack Vaan'
+  AND Email='jvaan@wisdompets.com'),
+  '25-08-2022','08-09-2022'),
+((SELECT BookID FROM Books 
+  WHERE Title='Great Expectations'
+  AND Barcode=4043822646),
+  (SELECT PatronID FROM Patrons 
+  WHERE FirstName||' '||LastName='Jack Vaan'
+  AND Email='jvaan@wisdompets.com'),
+  '25-08-2022','08-09-2022')
