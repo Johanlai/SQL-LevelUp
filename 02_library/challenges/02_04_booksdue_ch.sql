87cfdc674441b7aec6ff65be693fee7bc71a3c0c
Johanlai <65450101+Johanlai@users.noreply.github.com>
2023-07-04 13:16:44 +0000

    4-5

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,9 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Patrons ON Patrons.PatronID=Loans.PatronID
+JOIN Books on Loans.BookID=Books.BookID
+WHERE Loans.DueDate='2022-07-13';
--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,20 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+
+SELECT Loans.LoanID, Loans.ReturnedDate
+FROM Books
+JOIN Loans ON Loans.BookID = Books.BookID
+WHERE Books.Barcode IN (6435968624, 5677520613, 8730298424)
+AND Loans.ReturnedDate IS '2022-07-05';
+
+UPDATE Loans
+SET ReturnedDate= DATE('2022-07-05')
+WHERE LoanID in (
+  SELECT Loans.LoanID
+  FROM Books
+  JOIN Loans ON Loans.BookID = Books.BookID
+  WHERE Books.Barcode IN (6435968624, 5677520613, 8730298424)
+  AND Loans.ReturnedDate IS NULL
+);
